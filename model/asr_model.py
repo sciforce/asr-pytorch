Fixed greedy search inference and edit distance calculation.

model/asr_model.py
@@ -15,7 +15,7 @@ class ASRTransformerModel(torch.nn.Module):
         super(ASRTransformerModel, self).__init__()
         self.params = params
         self.max_len_src = max_len_src
-        self.max_len_tgt = max_len_src
+        self.max_len_tgt = max_len_tgt
         self.n_outputs = n_outputs
         self.mfcc = MFCCLayer(params.sample_rate, params.n_mfcc,
                               params.n_fft, params.hop_length,
@@ -95,7 +95,7 @@ class ASRTransformerModel(torch.nn.Module):
         Args:
         x: input waveforms, tensor of size [batch x max_samples_count]
         x_lengths: number of samples in the inputs, tensor of size [batch]
-        targets: target embeddings, tensor of size [batch x max_target_sequence_length x target_dim]
+        targets: target embeddings, tensor of size [batch x max_target_sequence_length]
         target_lengths: lengths of target sequences, tensor of size [batch]
         Returns:
         outputs: tensor of size [batch x max_target_sequence_length x target_dim]
@@ -114,12 +114,14 @@ class ASRTransformerModel(torch.nn.Module):
         Args:
         x: input waveforms, tensor of size [batch x max_samples_count]
         x_lengths: number of samples in the inputs, tensor of size [batch]
-        partial targets: begginings of output sequence, tensor of size [batch x max_partial_target_sequence_length x target_dim]
+        partial targets: begginings of output sequence, tensor of size [batch x max_partial_target_sequence_length]
         partial_target_lengths: lengths of partial target sequences, tensor of size [batch]
         eos: end-of-sequence id
         """
         # TODO: implement beam search decoder
         # TODO: implement binary features decoder
+
+        partial_targets = self.embedding_layer(partial_targets)
 
         # Partial targets: batch x time x target_dim -> time x batch x target_dim
         partial_targets.transpose_(0, 1)
@@ -128,12 +130,9 @@ class ASRTransformerModel(torch.nn.Module):
         for _ in range(max_len, self.max_len_tgt):
             output = self._decoder_step(memory, partial_targets, partial_target_lengths,
                                         memory_mask, memory_key_padding_mask)
-            out = output[-1, ...]
-            out = torch.nn.functional.one_hot(torch.argmax(out, dim=-1),
-                                              num_classses=self.n_outputs).unsqueeze(0)
-            if eos is not None and (out == eos).all():
+            output = torch.argmax(output, dim=-1)
+            if eos is not None and (output[-1, ...] == eos).all():
                 break
-            partial_targets = torch.cat((partial_targets, out.unsqueeze(0)), dim=0)
-        # Output: time x batch x target_dim -> batch x time x target_dim
-        output.transpose_(0, 1)
+            out = self.embedding_layer(output)
+            partial_targets = torch.cat((partial_targets[:1, ...], out.transpose(0, 1).detach()), dim=0).detach()
         return output

test/edit_distance.py
@@ -1,14 +1,14 @@
 import torch
-from utils.ipa_encoder import EOS_ID
+from utils.ipa_encoder import SOS_ID, EOS_ID
 
 
 def get_seq_len(x, eos_id=EOS_ID):
     mask = x == eos_id
     inds = (torch.arange(x.size(1), device=x.device)
             .unsqueeze(0)
-            .repeat(x.size(0)))
+            .repeat(x.size(0), 1))
     inds.masked_fill_(mask, -1)
-    return inds.max(dim=1) + 1
+    return inds.max(dim=1).values + 1
 
 
 def edit_distance(outputs, targets, eos_id=EOS_ID,
@@ -19,27 +19,40 @@ def edit_distance(outputs, targets, eos_id=EOS_ID,
     D = torch.zeros((batch_size, max_output_len, max_target_len),
                     device=outputs.device)
     D[:, 1:, 0] = (torch.arange(1, max_output_len)
-                   .unsqueeze(1).unsqueeze(0)
-                   .repeat(batch_size, 1, max_target_len)
+                   .unsqueeze(0)
+                   .repeat(batch_size, 1)
                    .to(outputs.device)) * w_ins
     D[:, 0, 1:] = (torch.arange(1, max_target_len)
-                   .unsqueeze(0).unsqueeze(0)
-                   .repeat(batch_size, max_output_len, 1)
+                   .unsqueeze(0)
+                   .repeat(batch_size, 1)
                    .to(outputs.device)) * w_del
     for i in range(1, max_output_len):
         for j in range(1, max_target_len):
             d_same = D[:, i - 1, j - 1]
-            d_different = torch.stack((D[:, i - 1, j] + w_ins,
-                                       D[:, i, j - 1] + w_del,
-                                       D[:, i, j] + w_sub), dim=4).min(dim=-1)
-            D[:, i, j] = torch.where(outputs[:, i] == targets[:, j], d_same, d_different)
+            d_different = torch.stack((D[:, i - 1, j].unsqueeze(-1) + w_ins,
+                                       D[:, i, j - 1].unsqueeze(-1) + w_del,
+                                       D[:, i - 1, j - 1].unsqueeze(-1) + w_sub), dim=-1).min(dim=-1).values
+            D[:, i, j] = torch.where(outputs[:, i] == targets[:, j], d_same, d_different.squeeze(-1))
     output_last_inds = get_seq_len(outputs, eos_id) - 1
     target_last_inds = get_seq_len(targets, eos_id) - 1
-    batch_inds = torch.arange(0, batch_size, dtype=torch.long).to(ouputs.device)
+    batch_inds = torch.arange(0, batch_size, dtype=torch.long).to(outputs.device)
     index_vector = torch.stack((batch_inds, output_last_inds,
                                 target_last_inds), dim=-1)
     index_vector = (index_vector[:, 0] * max_output_len * max_target_len
                     + index_vector[:, 1] * max_target_len
                     + index_vector[:, 2])
-    edit_distances = D.take(index_vector) / (target_last_inds + 1)
+    edit_distances = D.take(index_vector) / target_last_inds
     return edit_distances
+
+
+if __name__ == '__main__':
+    x = 'Hell, ward.'
+    y = 'Hello, world.'
+    vocab = {'SOS': SOS_ID, 'EOS': EOS_ID}
+    for c in x + y:
+        if c not in vocab:
+            vocab[c] = len(vocab)
+    x = torch.Tensor([SOS_ID] + [vocab[c] for c in x] + [EOS_ID, EOS_ID]).unsqueeze(0)
+    y = torch.Tensor([SOS_ID] + [vocab[c] for c in y] + [EOS_ID, EOS_ID, EOS_ID]).unsqueeze(0)
+    d = edit_distance(x, y)
+    print(f'edit distance is {d}')

test/run_test.py
@@ -24,7 +24,7 @@ def run_test(test_data, device, n_outputs,
     distances = []
     for batch in pbar:
         x, x_lengths, targets, _ = [x.to(device) for x in batch]
-        partial_targets = torch.full((x.size(0), 1), SOS_ID, device=device)
+        partial_targets = torch.full((x.size(0), 1), SOS_ID, device=device, dtype=torch.long)
         partial_target_lengths = torch.ones((x.size(0),), device=device, dtype=torch.long)
         outputs = model.inference(x, x_lengths, partial_targets, partial_target_lengths, eos=EOS_ID)
         distances.append(edit_distance(outputs, targets, EOS_ID).detach())
@@ -42,7 +42,7 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', type=int, default=64,
                         help='Test batch size')
     args = parser.parse_args()
-    test_data = load_dataset(Path(args.data_dir), subset='dev')
+    test_data = load_dataset(Path(args.data_dir), subset='train')
     if torch.cuda.is_available():
         device = 'cuda'
         logger.debug('Using CUDA')